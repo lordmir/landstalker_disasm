5200600d4e08b85cfe034ed23578e6d6fcd3480c
hase <hase@redfern.xyz>
2024-12-10 01:13:38 +0000

    Revert "Use `wineconsole` rather than `wine` for running assembler."
    This reverts commit 753a8273d978fc3d53489952e51cab809e7534bb.

--- build_expanded.sh
+++ build_expanded.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wineconsole tools/build/asm68k.exe /p /o ae-,e+,w+,c+,op+,os+,ow+,oz+,l_ /e EXPANDED=1 landstalker_us_expanded.asm,landstalker_expanded.bin
+wine tools/build/asm68k.exe /p /o ae-,e+,w+,c+,op+,os+,ow+,oz+,l_ /e EXPANDED=1 landstalker_us_expanded.asm,landstalker_expanded.bin
 python3 tools/build/fix_checksum.py landstalker_expanded.bin
