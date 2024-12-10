753a8273d978fc3d53489952e51cab809e7534bb
hase <hase@redfern.xyz>
2024-12-10 01:08:35 +0000

    Use `wineconsole` rather than `wine` for running assembler.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wine tools/build/asm68k.exe /p /o ae-,e+,w+,c+,op+,os+,ow+,oz+,l_ /e EXPANDED=0 landstalker_us.asm,landstalker.bin
+wineconsole tools/build/asm68k.exe /p /o ae-,e+,w+,c+,op+,os+,ow+,oz+,l_ /e EXPANDED=0 landstalker_us.asm,landstalker.bin
 python3 tools/build/fix_checksum.py landstalker.bin
 
