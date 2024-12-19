caac6d2d0b726d0b2524b2eaf459ab20a5882e0a
hase <hase@redfern.xyz>
2024-12-19 20:13:53 +0000

    Add label for global defines Consolidate Friday animation tables Remove unnecessary align directives

--- landstalker_us_expanded.asm
+++ landstalker_us_expanded.asm
@@ -5,7 +5,7 @@
 ;; To build, run:
 ;; .\build\asm68k.exe /p /o ae-,e+,w+,c+,op+,os+,ow+,oz+,l_ /e EXPANDED=1 landstalker_expanded.asm,landstalker_expanded.bin
 
-                            include "code\include\landstalker.inc"
+Defines:                    include "code\include\landstalker.inc"
                             include "code\include\ram.inc"
                             include "code\include\macros.inc"
 
@@ -22,47 +22,13 @@ LavaPaletteRotation:        incbin  "assets_packed\graphics\miscpalettes\lavapal
 LabrynthLitPal:             incbin  "assets_packed\graphics\miscpalettes\labrynthlit.pal"
                             include "code\gamelogic\gamelogic2.asm"
                             include "code\misc\fridayanimation.asm"
-FridayAnimation1:           incbin  "assets_packed\misc\fridayanim\fridayanim01.bin"
-                            Align   $2
-FridayAnimation2:           incbin  "assets_packed\misc\fridayanim\fridayanim02.bin"
-                            Align   $2
-FridayAnimation3:           incbin  "assets_packed\misc\fridayanim\fridayanim03.bin"
-                            Align   $2
-FridayAnimation4:           incbin  "assets_packed\misc\fridayanim\fridayanim04.bin"
-                            Align   $2
-FridayAnimation5:           incbin  "assets_packed\misc\fridayanim\fridayanim05.bin"
-                            Align   $2
-FridayAnimation6:           incbin  "assets_packed\misc\fridayanim\fridayanim06.bin"
-                            Align   $2
-FridayAnimation7:           incbin  "assets_packed\misc\fridayanim\fridayanim07.bin"
-                            Align   $2
-FridayAnimation8:           incbin  "assets_packed\misc\fridayanim\fridayanim08.bin"
-                            Align   $2
-FridayAnimation9:           incbin  "assets_packed\misc\fridayanim\fridayanim09.bin"
-                            Align   $2
-FridayAnimation10:          incbin  "assets_packed\misc\fridayanim\fridayanim10.bin"
-                            Align   $2
-FridayAnimation11:          incbin  "assets_packed\misc\fridayanim\fridayanim11.bin"
-                            Align   $2
-FridayAnimation12:          incbin  "assets_packed\misc\fridayanim\fridayanim12.bin"
-                            Align   $2
-FridayAnimation13:          incbin  "assets_packed\misc\fridayanim\fridayanim13.bin"
-                            Align   $2
-FridayAnimation14:          incbin  "assets_packed\misc\fridayanim\fridayanim14.bin"
-                            Align   $2
-FridayAnimation15:          incbin  "assets_packed\misc\fridayanim\fridayanim15.bin"
-                            Align   $2
+FridayAnimationData:        include "code\misc\fridayanimationdata.asm"
                             include "code\gamelogic\gamelogic3.asm"
                             include "code\maps\mapflagchanges.asm"
-                            Align   $2
 RoomGfxSwapFlags:           incbin  "assets_packed\roomdata\flagactions\roomgfxswapflags.bin"
-                            Align   $2
 LockedDoorGfxSwapFlags:     incbin  "assets_packed\roomdata\flagactions\lockeddoorgfxswapflags.bin"
-                            Align   $2
 TreeWarpGfxSwapFlags:       incbin  "assets_packed\roomdata\flagactions\treewarpgfxswapflags.bin"
-                            Align   $2
 TileSwaps:                  incbin  "assets_packed\roomdata\flagactions\tileswaps.bin"
-                            Align   $2
                             include "code\gamelogic\gamelogic4.asm"
                             include "code\inventory\inventory1.asm"
 InvItemPal:                 incbin  "assets_packed\graphics\static\inventory\invitempal.pal"
@@ -88,38 +54,29 @@ StatusBarGfx:               incbin  "assets_packed\graphics\static\hud\hud.lz77"
                             Align   $2
                             include "code\maps\doors.asm"
 DoorLookup:                 incbin  "assets_packed\roomdata\misc\doorlookup.bin"
-                            Align   $2
 DoorTable:                  incbin  "assets_packed\roomdata\misc\doortable.bin"
                             Align   $2
                             include "code\gamelogic\shops\shops1.asm"
 Shops:                      incbin  "assets_packed\roomdata\misc\shops.bin"
-                            Align   $2
                             include "code\gamelogic\shops\shops2.asm"
 LifestockSoldFlags:         incbin  "assets_packed\roomdata\flagactions\lifestocksoldflags.bin"
-                            Align   $2
                             include "code\maps\animtilesets.asm"
 AnimatedTilesetTable:       include "code\maps\animtilesettbl.asm"
                             include "code\maps\room1.asm"
 RoomFallDestination:        incbin  "assets_packed\roomdata\warps\roomfalldests.bin"
-                            Align   $2
 RoomClimbDestination:       incbin  "assets_packed\roomdata\warps\roomclimbdests.bin"
-                            Align   $2
 RoomTransitionLookup:       incbin  "assets_packed\roomdata\flagactions\roomtransitions.bin"
-                            Align   $2
                             include "code\blocks\loadisometricblocks1.asm"
 VDPCopyBlockCmds:           incbin  "assets_packed\misc\data\vdpcopyblockcmds.bin"
 TopLeftMapOffsets:          incbin  "assets_packed\misc\data\ulmapoffsets.bin"
 BottomMapOffsets:           incbin  "assets_packed\misc\data\dmapoffsets.bin"
 RightMapOffsets:            incbin  "assets_packed\misc\data\rmapoffsets.bin"
-                            Align   $2
                             include "code\blocks\loadisometricblocks2.asm"
 IntroStrings:               include "code\text\introstrings.asm"
 RoomVisitedFlagLookup:      incbin  "assets_packed\roomdata\flagactions\roomvisitflags.bin"
-                            Align   $2
                             include "code\maps\mapdecompress.asm"
                             include "code\blocks\blockdecompress.asm"
                             include "code\inventory\inventory3.asm"
-                            Align   $2
 InventoryItems:             incbin  "assets_packed\misc\inventory\inventoryitems.bin"
                             Align   $2
                             include "code\inventory\inventory4.asm"
@@ -136,10 +93,8 @@ GameLoadScreen:             include "code\title\gameloadscreen.asm"
                             include "code\sprites\spritefuncsjumptable.asm"
                             include "code\sprites\spritefuncs1.asm"
 SpriteAnimFlags:            incbin  "assets_packed\spritedata\spriteanimflags.bin"
-                            Align   $2
                             include "code\gamelogic\playbackinput.asm"
 InputPlayback:              incbin  "assets_packed\misc\playbackinputtable.bin"
-                            Align   $2
                             include "code\sprites\spritefuncs2.asm"
 BehaviourTriggerJumpTable:  include "code\scripthandling\triggeractionjumptable.asm"
 BehaviourTriggerFuncs:      include "code\scripthandling\triggeractions.asm"
@@ -157,28 +112,20 @@ StatusAnimData:             include "code\graphics\staticimages\statusfx.asm"
                             include "code\maps\loadroomsprites.asm"
 CustomRoomActions1:         include "code\maps\customroomactions1.asm"
 BigTreeLocations:           incbin  "assets_packed\roomdata\misc\bigtreelocs.bin"
-                            Align   $2
 CustomRoomActions2:         include "code\maps\customroomactions2.asm"
                             include "code\maps\roomspriteflags.asm"
 SpriteVisibilityFlags:      incbin  "assets_packed\roomdata\flagactions\spritevisibility.bin"
-                            Align   $2
 OneTimeEventFlags:          incbin  "assets_packed\roomdata\flagactions\onetime.bin"
-                            Align   $2
 RoomClearFlags:             incbin  "assets_packed\roomdata\flagactions\roomcleared.bin"
-                            Align   $2
 LockedDoorSpriteFlags:      incbin  "assets_packed\roomdata\flagactions\lockeddoorsprites.bin"
-                            Align   $2
 PermanentSwitchFlags:       incbin  "assets_packed\roomdata\flagactions\permanentswitches.bin"
-                            Align   $2
 SacredTreeFlags:            incbin  "assets_packed\roomdata\flagactions\sacredtrees.bin"
 SpriteGfxIdxLookup:         incbin  "assets_packed\spritedata\spritegfx.bin"
 SpriteDimensionsLookup:     incbin  "assets_packed\spritedata\spritedimensions.bin"
                             Align   $2
 RoomSpriteTableOffset:      incbin  "assets_packed\spritedata\roomtableoffsets.bin"
 EnemyStats:                 incbin  "assets_packed\spritedata\enemystats.bin"
-                            Align   $2
 RoomSpriteTable:            incbin  "assets_packed\spritedata\roomspritetable.bin"
-                            Align   $2
 StringPtrs:                 include "code\pointertables\strings\stringbankptr.asm"
                             include "code\text\stringfuncsjumptable.asm"
                             include "code\text\stringfuncs1.asm"
@@ -202,7 +149,6 @@ ScriptProgressFlags:        include "code\script\scriptflagcheck.asm"
                             include "code\scripthandling\scriptfuncs3.asm"
 CutsceneScriptTable:        include "code\script\cutscenes\script_cutscenetable.asm"
 RoomDialogueTable:          incbin  "assets_packed\roomdata\roomcharatertable.bin"
-                            Align   $2
 CharacterScriptTable:       include "code\script\characters\script_charactertable.asm"
                             include "code\scripthandling\scriptfuncs4.asm"
 ShopScriptFuncs:            include "code\script\shops\script_shops.asm"
@@ -210,25 +156,19 @@ ShopScriptFuncs:            include "code\script\shops\script_shops.asm"
 CharacterScriptFuncs:       include "code\script\characters\script_characters.asm"
 CutsceneScriptFuncs:        include "code\script\cutscenes\script_cutscenes.asm"
 Script:                     incbin  "assets_packed\script\script.bin"
-                            Align   $2
                             include "code\scripthandling\scriptfuncs6.asm"
 SpriteIdToTalkSfx:          incbin  "assets_packed\script\spritetalksfx.bin"
-                            Align   $2
                             include "code\scripthandling\scriptfuncs7.asm"
 SpecialCharacterSfxList:    incbin  "assets_packed\script\charactertalksfx.bin"
-                            Align   $2
                             include "code\scripthandling\scriptfuncs8.asm"
 ItemProperties:             incbin  "assets_packed\script\items.bin"
-                            Align   $2
                             include "code\scripthandling\scriptfuncs9.asm"
 SaveGameLocations:          incbin  "assets_packed\script\savegamelocations.bin"
-                            Align   $2
                             include "code\scripthandling\islandmaplocations.asm"
 IslandMapLocations:         incbin  "assets_packed\script\islandmaplocations.bin"
 StringTables:               include "code\text\stringtables.asm"
                             include "code\scripthandling\script_visualfx.asm"
 SineTable:                  incbin  "assets_packed\misc\data\sinetable.bin"
-                            Align   $2
                             include "code\misc\debugmenu.asm"
 StringData:                 include "code\text\strings.asm"
                             Align   $2
@@ -265,11 +205,9 @@ RoomData_0:                 include "code\pointertables\maps\roomlist.asm"
 RoomMaps:                   include "code\graphics\roommaps.asm"
 RoomPals:                   include "code\palettes\roompals.asm"
 RoomExits:                  incbin  "assets_packed\roomdata\warps\exits.bin"
-                            Align   $2
 RegionCheck:                include "code\system\regioncheck.asm"
                             include "code\pointertables\sprites\spritegraphicsptr.asm"
 SpriteGfxOffsetTable:       incbin  "assets_packed\spritedata\spritegfxoffsettable.bin"
-                            Align   $2
 SpriteAnimationPtrs:        include "code\pointertables\sprites\spriteanimations.asm"
 SpriteFramePtrs:            include "code\pointertables\sprites\spriteanimationframes.asm"
 SpriteFrames:               include "code\sprites\spriteframes.asm"
