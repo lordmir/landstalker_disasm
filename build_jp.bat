dbf58e595b996a0e76693d615b7d417a124eb266
hase <hase@redfern.xyz>
2025-01-04 15:17:42 +0000

    Combine build scripts into singular file, add expanded JP ROM assembly

--- build_jp.bat
+++
@@ -1,4 +0,0 @@
-tools\build\asm68k.exe /p /o ae-,e+,w+,c+,op+,os+,ow+,oz+,l_ /e EXPANDED=0 landstalker_jp.asm,landstalker_jp.bin
-python tools\build\fix_checksum.py landstalker_jp.bin
-pause
-
--- build_jp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wine tools/build/asm68k.exe /p /o ae-,e+,w+,c+,op+,os+,ow+,oz+,l_ /e EXPANDED=0 landstalker_jp.asm,landstalker_jp.bin
-python3 tools/build/fix_checksum.py landstalker_jp.bin
-
